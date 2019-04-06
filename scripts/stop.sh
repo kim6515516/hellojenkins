ac758f232a44efbbb83e944a93c62de6f57e8edd
kim6515516 <kim6515516@gmail.com>
2019-04-06 11:06:37 +0000

    add files *

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,1 +1,5 @@
-stop helloworld
+#!/bin/sh
+
+[[ -e /etc/init/helloworld.conf ]] && \
+  status helloworld | grep -q '^helloworld start/running, process' && \
+    [[ $? -eq 0 ]] && stop helloworld || echo "Application not started"
